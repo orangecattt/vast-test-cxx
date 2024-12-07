fbc0c1b895974c2214c949728402f57acf9ba0e7
orangecattt <yibozh@126.com>
2024-12-07 13:21:47 +0800

    add TESTCASE into config

--- test.sh
+++ test.sh
@@ -17,7 +17,10 @@ cd ${BUILD_DIR}
 if [ -z ${LIT_BIN_NAME} ]; then
     LIT_BIN_NAME='lit'
 fi
-TESTCASE="."
+if [ -z ${TESTCASE} ]; then
+    TESTCASE='.'
+fi
+
 cmake .. -DVAST_PATH=${VAST_PATH} -DLIT_PATH=${LIT_PATH} -DLIT_BIN_NAME=${LIT_BIN_NAME} -DTESTCASE=${TESTCASE} &> /dev/null
 
 ctest &> /dev/null
